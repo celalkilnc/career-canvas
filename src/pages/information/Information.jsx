6551a434c3f67f9b101ce56628674c6bd7ac1f9f
celal.kilinc <celal.kilinc@santsg.com>
2024-02-09 13:52:23 +0300

    card added

--- src/pages/information/Information.jsx
+++ src/pages/information/Information.jsx
@@ -1,10 +1,14 @@
+import InformationCard from "../../components/InfoCard/InformationCard";
 import "./InformationStyle.css";
 import React from "react";
 
 function Information() {
   return (
     <div className="information" id="information">
-      Information
+      <div>
+        
+      </div>
+      <InformationCard data={{key:"test", value:"testfgdfgdfgtes"}}/>
     </div>
   );
 }
